fe79ccdca0832de02c9fa9cb6aa3606a8c1be6c3
karthikeyan Ramachandran <karthi1908@gmail.com>
2022-09-17 17:26:19 +0800

    Marketplace changes

--- contracts/CCMarketplace.sol
+++ contracts/CCMarketplace.sol
@@ -5,6 +5,7 @@ pragma solidity >=0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import '@openzeppelin/contracts/access/Ownable.sol';
 import './CCNFT.sol';
 
 
@@ -16,15 +17,32 @@ error NotOwner();
 error NotApprovedForMarketplace();
 error PriceMustBeAboveZero();
 
-contract CCMarketplace is ReentrancyGuard {
+contract CCMarketplace is ReentrancyGuard, Ownable{
     
     
     CCNFT public ccNFT;
+
+    struct CCredits{
+        address user;
+        uint quantity;
+
+    }
+
+    mapping(uint256 => CCredits) public creditDetails;
 
     function setCCNFTAddress(address _ccNFTAddress) public  onlyOwner {
         ccNFT = CCNFT(_ccNFTAddress);
     
     }
+
+    function listCCNFT(uint256 _tokenId) external {
+
+        uint creditQuantity = _tokenId % 1000;        
+        creditDetails[_tokenId] = CCredits( msg.sender , creditQuantity);
+        ccNFT.safeTransferFrom(msg.sender, address(this), _tokenId);
+
+    }
+
 
 
 }

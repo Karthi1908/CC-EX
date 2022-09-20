f97e612dcb37db141c46dedc611bd349fddc5869
karthikeyan Ramachandran <karthi1908@gmail.com>
2022-09-20 20:08:57 +0800

    UI for CC-EX

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,24 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import { MoralisProvider } from 'react-moralis'
 import Header from '../components/Header'
+const APP_ID = process.env.NEXT_PUBLIC_APP_ID;
+const SERVER_ID = process.env.NEXT_PUBLIC_MORALIS_SERVER
 
 function MyApp({ Component, pageProps }) {
   return (
-  <MoralisProvider initializeOnMount={false}>
-    <Header />
-    <Component {...pageProps} />
-  </MoralisProvider>
+    <div>
+      <Head>
+        <title>CC-EX</title>
+        <meta name="description" content="Carbon Credits Marketplace" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <MoralisProvider appId={APP_ID} serverUrl={SERVER_ID}>
+        <Header />
+        <Component {...pageProps} />
+      </MoralisProvider>
+    </div>
   )
 }
 
